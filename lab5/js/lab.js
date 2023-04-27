7673890ab2a81cb9273cec043dae09b362ee705a
sykuwaha <97131604+sykuwaha@users.noreply.github.com>
2023-04-26 23:29:14 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author:    Alexis Huang and Scott Kuwahara
+ * Created:   4.26.2023
+ * License: Public Domain
+ **/
 
-// Constants
 
-// Functions
+// Variables
+make = "Toyota";
+model = "Prius";
+color = "Gray";
+year = 2020;
+ownIt = false;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// calculate
+age = 2023 - year;
 
-// let's get this party started
-main();
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
